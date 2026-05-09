da469179a7a0c9cec9ac925f43f11b5e6a4473d4
Md Ashiq ilahi <128233331+ashiq012@users.noreply.github.com>
2026-05-09 10:59:51 +0530

    Rename Protected component import to match casing

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter} from 'react-router'
 import Login from './features/auth/pages/Login.jsx'
 import Register from './features/auth/pages/Register.jsx'
-import Protected from './features/auth/components/protected.jsx'
+import Protected from './features/auth/components/Protected.jsx'
 import Home from './features/interview/pages/Home.jsx'
 import Interview from './features/interview/pages/Interview.jsx'
 export const router = createBrowserRouter([
