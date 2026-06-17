c7188bed3db6edcc57cf4fd008a8546cb529e232
Md Ashiq ilahi <128233331+ashiq012@users.noreply.github.com>
2026-06-17 16:08:28 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ DB();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://genai-interviewprep.onrender.com",
     credentials:true
 }))
 
